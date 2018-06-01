b942a55a5155c80fdd6b70651e08fe40f6833b3f
Unknown <i0r67623@marymount.edu>
2018-06-01 08:44:10 -0400

    index

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>MUshuttleLess</title>
+</head>
+<body>
+<h1>Welcome to MU Shuttleless!</h1>
+
+
+
+</body>
+
+</html>
