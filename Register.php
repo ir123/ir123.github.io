082e6e0e9a845f0f8d01070d5d799adb71a3eb13
Unknown <i0r67623@marymount.edu>
2018-06-08 15:05:04 -0400

    updating

---
+++ Register.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<head>
+<title>Registation Page</title>
+<link rel="stylesheet" href="HomePage.css">
+</head>
+<body>
+  <h2>Register</h2></br></br>
+<form>
+<h3>Please enter your </h3></br>
+Email: <input type="text" name="Email"></br>
+Password: <input type="text" name="Password"></br>
+<!--Enter a php query to search for the email in the database, by
+turning the email input into a variable, then compare that variable
+with DB entries. Conditional if email found, then change the
+database record to validated-->
+
+<input type="submit" value="Registation">
+</form>
+
+
+</body>
---
+++ shuttleless.php
@@ -0,0 +1,15 @@
+<head>
+<link rel="stylesheet" href="HomePage.css">
+</head>
+<!--If the user chose the driver option display the drop down with waiting locations
+if the user is a passenger then display the option with pickup locations and
+give options for possible routes.
+
+***KEY***Will this matching be automatic or will it be on a refresh bases...
+I can do pick a route for passenger and it will match to driver ie it will give
+assign a pickup location. So the passenger has to refresh..-->. 
+<body>
+<h1>Welcome to the Shuttleless page</h1>
+
+
+</body>
